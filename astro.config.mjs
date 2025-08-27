90f6ddeabf23520dbd6113090cd01a29bdd63baa
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2025-08-27 12:34:15 -0700

    Remove unused stuff, add neodrag to window

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import unocss from "unocss/astro";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import expressiveCode from "astro-expressive-code";
-import rehypeSectionize from "@hbsnow/rehype-sectionize";
-import readingTime from "astro-reading-time";
-import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,18 +8,6 @@ export default defineConfig({
 		unocss({
 			injectReset: true,
 		}),
-		expressiveCode({
-			defaultProps: {
-				wrap: true,
-			},
-		}),
-		mdx(),
-		sitemap(),
-		readingTime(),
 	],
 	site: "https://ethanlew.dev",
-	markdown: {
-		remarkPlugins: [],
-		rehypePlugins: [rehypeHeadingIds, rehypeSectionize],
-	},
 });
